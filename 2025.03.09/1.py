96dac0149dbea7180938ab396e2ee64da9ad4c8f
Gennadiy <renardes@gmail.com>
2025-04-05 22:33:08 +0500

    2025.03.09: оформление

--- 2025.03.09/1.py
+++ 2025.03.09/1.py
@@ -5,18 +5,14 @@ name = input('Введите имя: ')
 surname = input('Введите фамилию: ')
 year = input('Введите год рождения: ')
 
-
 act_year = datetime.datetime.now().year
+n_year = act_year - int(year)
+
+print(name, ' ' , surname, ',' , ' ', n_year, sep='')
 
 
-n_year = (act_year) - int(year)
+# Введите имя: Максим
+# Введите фамилию: Герасимов
+# Введите год рождения: 2001
+# Максим Герасимов, 24
 
-
-print((name),' ' ,(surname),',' ,' ' ,(n_year), sep='')
-
-
-#C:\Users\gera\Desktop>python 1.py
-#Введите имя: Максим
-#Введите фамилию: Герасимов
-#Введите год рождения: 2001
-#Максим Герасимов, 24
--- 2025.03.09/4.py
+++ 2025.03.09/4.py
@@ -1,9 +1,8 @@
 number_n = input('Введите желаемое 3-x значное число: ')
 
-
 number_3 = int(number_n) // 100
-number_2 = (int(number_n) - (number_3) * 100) // 10
-number_1 = (int(number_n) - (number_3) * 100 - (number_2) * 10) // 1
+number_2 = (int(number_n) - number_3*100) // 10
+number_1 = (int(number_n) - number_3*100 - number_2*10) // 1
 
 summa = number_3 + number_2 + number_1
 isumma = int(summa)
@@ -11,13 +10,13 @@ isumma = int(summa)
 der = number_3 * number_2 * number_1
 ider = int(der)
 
-
 print(f"Сумма цифр = {isumma} {'\n'}Произведение цифр = {ider}")
 
 
-#C:\Users\gera\Desktop>python 4.py
-#Введите желаемое 3-x значное число: 202
-#Сумма цифр = 4
-#Произведение цифр = 0
+# Введите желаемое 3-x значное число: 202
+# Сумма цифр = 4
+# Произведение цифр = 0
 
-#инетресная задача с подвохом
+
+# инетресная задача с подвохом
+
