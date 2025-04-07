6fccd62b5b03242e9ab15cf4a128f527b1c8b22b
Gennadiy <renardes@gmail.com>
2025-04-07 20:11:22 +0500

    2025.03.17: оформление

--- 2025.03.17/1.py
+++ 2025.03.17/1.py
@@ -5,33 +5,30 @@ nm_1 = input('число 1: ')
 nm_2 = input('число 2: ')
 nm_3 = input('число 3: ')
 
-
 nm_1 = Decimal(nm_1)
 nm_2 = Decimal(nm_2)
 nm_3 = Decimal(nm_3)
 
-
 if nm_1 > 0:
     num_1 = nm_1 + 0
-elif nm_1 <=0:
+elif nm_1 <= 0:
     num_1 = 0
     
 if nm_2 > 0:
     num_2 = num_1 + nm_2
-elif nm_2 <=0:
+elif nm_2 <= 0:
     num_2 = num_1 + 0
     
 if nm_3 > 0:
     num_3 = num_2 + nm_3
 elif nm_3 <= 0:
     num_3 = num_2 + 0
-    
-    
+
 print('сумма положительных чисел', num_3)
 
 
-#C:\Users\gera\Desktop>python 1.py
-#число 1: 4
-#число 2: -4
-#число 3: 1.5
-#сумма положительных чисел 5.5
+# число 1: 4
+# число 2: -4
+# число 3: 1.5
+# сумма положительных чисел 5.5
+
--- 2025.03.17/3.py
+++ 2025.03.17/3.py
@@ -1,17 +1,15 @@
 yer = int(input('ваш год: '))
 
-
 if yer % 4 == 0 and yer / 100 > 0 or yer % 400 == 0:
     print('да')
-    
 else:
     print('нет') 
-    
-    
-#C:\Users\gera\Desktop>python 3.py
-#ваш год: 1941
-#нет
 
-#C:\Users\gera\Desktop>python 3.py
-#ваш год: 1940
-#да
+
+# ваш год: 1941
+# нет
+
+
+# ваш год: 1940
+# да
+
